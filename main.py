4c6c0c88dc180a98e04e993ce79e619d96457f62
nut <jackg2667@protonmail.com>
2023-08-01 16:36:04 +0100

    adding unit conversion and error handling

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-height = float(input("Enter the height of the room in your chosen measurement (cubic ft or metres): "))
-length = float(input("Enter the length of the room in your chosen measurement (cubic ft or metres): "))
-width = float(input("Enter the width of the room in your chosen measurement (cubic ft or metres): "))
+def get_valid_float_input(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+while True:
+    try:
+        units = input("Enter your desired measurement units (ft or metres): ").lower()
+        if units not in ['ft', 'metres']:
+            raise ValueError("Invalid units. Please enter 'ft' or 'metres'.")
+
+        height = get_valid_float_input(f"Enter the height of the room in {units}: ")
+        length = get_valid_float_input(f"Enter the length of the room in {units}: ")
+        width = get_valid_float_input(f"Enter the width of the room in {units}: ")
+
+        # Add any additional calculations or processing here
+
+        break  # Exit the loop if input is valid
+
+    except ValueError as e:
+        print(f"Error: {e}. Please try again.")
+
 
 result = height * length * width
-print(f"{result} BTUs (British Thermal Units)")
+
+if units == "metres":
+    result *= 35.3
+    print(f"{result} BTUs (British Thermal Units)")
+else:
+    print(f"{result} BTUs (British Thermal Units)")
