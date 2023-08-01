8c64e85d319da2f72fb566be67ccca3d27c38c76
nut <jackg2667@protonmail.com>
2023-08-01 17:10:36 +0100

    adding room types and formatting result

--- main.py
+++ main.py
@@ -6,28 +6,60 @@ def get_valid_float_input(prompt):
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-while True:
-    try:
-        units = input("Enter your desired measurement units (ft or metres): ").lower()
+
+def get_measurement_units():
+    while True:
+        units = input(
+            "Enter your desired measurement units (ft or metres): ").lower()
         if units not in ['ft', 'metres']:
-            raise ValueError("Invalid units. Please enter 'ft' or 'metres'.")
-
-        height = get_valid_float_input(f"Enter the height of the room in {units}: ")
-        length = get_valid_float_input(f"Enter the length of the room in {units}: ")
-        width = get_valid_float_input(f"Enter the width of the room in {units}: ")
-
-        # Add any additional calculations or processing here
-
-        break  # Exit the loop if input is valid
-
-    except ValueError as e:
-        print(f"Error: {e}. Please try again.")
+            print("Invalid units. Please enter 'ft' or 'metres'.")
+        else:
+            return units
 
 
-result = height * length * width
+def get_room_type():
+    while True:
+        room_type = input(
+            "Enter the room type you are measuring (Lounge, Dining Room, Bedroom, Hallway, Kitchen, and Bathroom): ").lower()
+        if room_type not in ['lounge', 'dining room', 'bedroom', 'hallway', 'kitchen', 'bathroom']:
+            print("Invalid room type. Please enter a valid room type.")
+        else:
+            return room_type
 
-if units == "metres":
-    result *= 35.3
-    print(f"{result} BTUs (British Thermal Units)")
-else:
-    print(f"{result} BTUs (British Thermal Units)")
+
+def calculate_btu(units, room_type, height, length, width):
+    result = height * length * width
+
+    if room_type in ['lounge', 'dining room', 'bathroom']:
+        result *= 5
+
+    if room_type in ['bedroom']:
+        result *= 4
+
+    if room_type in ['kitchen', 'hallway']:
+        result *= 3
+
+    if units == "metres":
+        result *= 35.3
+
+    return result
+
+
+def main():
+    units = get_measurement_units()
+    room_type = get_room_type()
+
+    height = get_valid_float_input(
+        f"Enter the height of the room in {units}: ")
+    length = get_valid_float_input(
+        f"Enter the length of the room in {units}: ")
+    width = get_valid_float_input(f"Enter the width of the room in {units}: ")
+
+    result = calculate_btu(units, room_type, height, length, width)
+    result_str = "{:.1f}".format(result)
+    
+    print(f"{result_str} BTUs (British Thermal Units)")
+
+
+if __name__ == "__main__":
+    main()
